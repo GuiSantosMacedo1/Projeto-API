e64ab715c40314fe03c9c34b78069a7dba82320e
Guilherme Santos <110691686+GuiSantosMacedo1@users.noreply.github.com>
2023-01-20 12:50:12 -0300

    Back-End
    Toda API funcionando com o Banco de Dados

---
+++ API JAVA/projeto/projeto/src/main/java/br/com/api/projeto/ProjetoApplication.java
@@ -0,0 +1,15 @@
+package br.com.api.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApplication.class, args);
+	}
+
+}
